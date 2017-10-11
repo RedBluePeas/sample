0ac3fbd2de3c97b617de2026bbf4ef1947fd45b3
shadow <352838184@qq.com>
2017-10-11 08:39:33 +0000

    Finish login and logout

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/about','StaticPagesController@about')->name('about');
 /* 注册 */
 Route::get('/signup','UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
+/* 登录退出 */
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
